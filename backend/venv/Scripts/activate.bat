3f60c06c5505887642e4008f8cef4fbc4fbbfd23
Ibraz <ibrazsiddique424@gmail.com>
2025-08-29 17:35:04 +0500

    Changes

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\inventory-management-system\backend\venv
+set VIRTUAL_ENV=D:\inventory_management_system\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
